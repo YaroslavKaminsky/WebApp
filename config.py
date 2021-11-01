cfa92ab7bd616150aecbd1f14b85f2c2ec76931b
YaroslavKaminsky <kaminsky.yarik@gmail.com>
2021-11-01 17:20:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-from boto.s3.connection import S3Connection
+from boto3 import S3Connection
 
 EXCHANGERATE_KEY = S3Connection(os.environ['EXCHANGERATE_KEY'])
 TELEGRAMBOT_KEY = S3Connection(os.environ['TELEGRAMBOT_KEY'])
