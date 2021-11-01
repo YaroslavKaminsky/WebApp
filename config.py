747ed2172bcae1a0443eb42179ec412eca799273
YaroslavKaminsky <kaminsky.yarik@gmail.com>
2021-11-01 17:13:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
+from boto.s3.connection import S3Connection
 
-EXCHANGERATE_KEY = os.environ.get('EXCHANGERATE_KEY', None)
-TELEGRAMBOT_KEY = os.environ.get('TELEGRAMBOT_KEY', None)
-DATABASE_URL = os.environ.get('DATABASE_URL', None)
+EXCHANGERATE_KEY = S3Connection(os.environ['EXCHANGERATE_KEY'])
+TELEGRAMBOT_KEY = S3Connection(os.environ['TELEGRAMBOT_KEY'])
+DATABASE_URL = S3Connection(os.environ['DATABASE_URL'])
 
 if __name__ == '__main__':
     print(EXCHANGERATE_KEY)
